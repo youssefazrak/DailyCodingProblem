c90f3eccde2d6102c707484bc45a787e676c60c0
Youssef <git@youssefazrak.com>
2018-10-28 20:25:41 +0000

    Second exercice - no division

---
+++ DCP_day2.py
@@ -0,0 +1,31 @@
+#!/bin/bash/python3
+
+"""
+Given an array of integers, return a new array such that each element at index i of the new array is the product of all the numbers in the original array except the one at i.
+
+For example, if our input was [1, 2, 3, 4, 5], the expected output would be [120, 60, 40, 30, 24]. If our input was [3, 2, 1], the expected output would be [2, 3, 6].
+
+Follow-up: what if you can't use division?
+"""
+# This solution provides no division. At this point I haven't found a proper solution to use with division. Probably related to the length that you divide.
+# In all cases this one seems clean. We basically separate the list into two: one before the index and one after the index. We multiple the elements of the first list between themselves till the index minus one. We do the same for the second list. In the end we multiply both results together and we have the final results.
+
+
+a = [1, 2, 3, 4, 5]
+#a = [3, 2, 1]
+b = []
+
+for i in range(0,len(a)):
+    v = 0
+    v1 = 1 
+    v2 = 1 
+
+    for j in range(0, i):
+        v1 = v1 * a[j]
+    for j in range(i+1, len(a)):
+        v2 = v2 * a[j]
+
+    v = v1 * v2
+    b.insert(i, v)
+
+print(b)
